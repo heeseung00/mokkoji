b754ebaa7f9420d9ddd75d2df970559e323fb321
Lee <smmang@gmail.com>
2024-05-02 18:04:49 +0900

    베스트 리뷰 서브 페이지 반응형 작성중

---
+++ js/leesub-best.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", function () {
+  // window.addEventListener("resize", function() {
+  //     var mapContainer = document.getElementById("daumRoughmapContainer1714381151460"); // 맵 컨테이너 요소 가져오기
+  //     var newWindowWidth = window.innerWidth - 1; // 새 창의 너비 계산
+  //     var newMapWidth = newWindowWidth - 50; // 맵 요소의 새 너비 계산
+  //     if (newWindowWidth <= 840) {
+  //       mapContainer.style.width = newMapWidth + "px"; // 맵 컨테이너의 너비 설정
+  //       mapContainer.setAttribute("data-width", newMapWidth); // 데이터 너비 설정
+  //     }
+  //   });
+});
